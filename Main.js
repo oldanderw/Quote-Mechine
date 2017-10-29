66e9bbe7491a5ecd5633ca49de3800c838c47b5d
David parker <adparker@fullsail.edu>
2017-10-29 01:56:38 -0400

    Node style lint

--- Main.js
+++ Main.js
@@ -1,63 +1,58 @@
-var myJson = Json;
-var x = 0;
-var lenght = 5;
-var quote = myJson.allQuotes[0].quote;
-var author = myJson.allQuotes[0].author;
-var signin = new SignIn();
-document.getElementById("mybtn").addEventListener("click", AddtoGen);
+const myJson = Json
+let x = 0
+let lenght = 5
+let quote = myJson.allQuotes[0].quote
+let author = myJson.allQuotes[0].author
+const signin = new SignIn()
+document.getElementById('mybtn').addEventListener('click', AddtoGen)
 
 
-//display first
-document.getElementById('quote').innerHTML='"' + quote + '"';
-document.getElementById('author').innerHTML="-" + author;
-console.log("Quote number 0 is being display.");
-setInterval(generateQuote, 7000);
+// Display first
+document.getElementById('quote').innerHTML = `$(quote)"`
+document.getElementById('author').innerHTML =
+setInterval(generateQuote, 7000)
 
 function generateQuote() {
+  x = ~~(Math.random() * lenght)
+  console.log(`Quote number $(x) is being display.`)
+  quote = myJson.allQuotes[x].quote
+  author = myJson.allQuotes[x].author
+  document.getElementById('quote').innerHTML = `"$(quote)"`
+  document.getElementById('author').innerHTML = `- $(author)`
+}
 
-    x = ~~(Math.random() * lenght)
-    console.log("Quote number " + x + " is being display.");
-    if (x < 0 || x > lenght) {
-        console.error("Quote number " + x + " does not exist." );
+
+function Add() {
+  var newQuote = prompt('what is the quote you would like to add?')
+  var newAuthor = prompt('who was this by?')
+  myJson.allQuotes[lenght] = {
+      quote: newQuote,
+      author: newAuthor,
     }
-    quote = myJson.allQuotes[x].quote;
-    author = myJson.allQuotes[x].author;
-    document.getElementById('quote').innerHTML='"' + quote + '"';
-    document.getElementById('author').innerHTML="-" + author;
-  };
-
-
-function Add(){
-    var newQuote = prompt("what is the quote you would like to add?");
-    var newAuthor = prompt("who was this by?");
-    myJson.allQuotes[lenght]= {
-        quote : newQuote,
-        author  : newAuthor
-    };
-    lenght++
+  lenght++
 }
 
 function AddtoGen() {
-    var login = 1;
-  while(login){
-  var user = prompt("what is your username?");
-  if(user == signin.admin){
-        var pass = prompt("what is your password?");
-        pass == signin.password ? Add()  : alert("the user name or password was incorrect.")
-        if (pass == signin.password){
-            break;
-       }
-    } else if (user == ""){
-      console.log("Login procces cancaled");
-      break;
+  let login = 1
+  while (login) {
+    let user = prompt('what is your username?')
+    if (user == signin.admin) {
+      let pass = prompt('what is your password?')
+      pass == signin.password ? Add()  : alert(`the user name or password was
+         incorrect.`)
+      if (pass == signin.password) {
+        break
+      }
+    } else if (user == '') {
+      break
 
     }
-  };
+  }
 }
 
 
-function SignIn(){
-    this.admin = "Admin";
-    this.password = "root";
+function SignIn() {
+  this.admin = 'Admin'
+  this.password = 'root'
 
 }
